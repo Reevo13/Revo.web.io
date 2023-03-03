7524d6192119247f37ea170203089576b791b968
Reevo13 <121067601+Reevo13@users.noreply.github.com>
2023-03-04 02:14:55 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,116 @@
+const body = document.body
+const pp = document.getElementById('pp')
+const fig = document.getElementById('fig')
+const fwa = document.getElementById('fwa')
+const dm = document.getElementById('dm')
+const rb = document.getElementById('rb')
+const pst = document.getElementById('pst')
+const str = document.getElementById('str')
+
+//body
+body.style.background = 'black'
+//pp
+pp.style.padding = '0px'
+pp.style.width = '100px'
+pp.style.border = '3px solid'
+pp.style.borderColor = 'gray'
+pp.style.background = ''
+pp.style.borderRadius = '50%'
+pp.style.position = 'relative'
+pp.style.left = '120px'
+pp.style.top = '35px'
+
+//figc
+fig.style.width = '30px'
+fig.style.position = 'relative'
+fig.style.left = '99px'
+fig.style.top = '-40px'
+
+//fwa
+fwa.style.width = '30px'
+fwa.style.position = 'relative'
+fwa.style.left = '99px'
+fwa.style.top = '-40px'
+
+//fyt
+fyt.style.width = '30px'
+fyt.style.position = 'relative'
+fyt.style.left = '99px'
+fyt.style.top = '-40px'
+
+//fyt
+fdc.style.width = '30px'
+fdc.style.position = 'relative'
+fdc.style.left = '99px'
+fdc.style.top = '-40px'
+
+//fgmm
+fgm.style.width = '30px'
+fgm.style.position = 'relative'
+fgm.style.left = '99px'
+fgm.style.top = '-40px'
+
+//dm
+dm.style.width = '30px'
+dm.style.position = 'relative'
+dm.style.left = '75px'
+dm.style.top = '-40px'
+
+//rb
+rb.style.width = '30px'
+rb.style.position = 'relative'
+rb.style.left = '80px'
+rb.style.top = '-40px'
+
+//pst
+pst.style.width = '35px'
+pst.style.position = 'relative'
+pst.style.left = '77px'
+pst.style.top = '-45px'
+
+//jki
+jki.style.width = '30px'
+jki.style.position = 'relative'
+jki.style.left = '85px'
+jki.style.top = '-40px'
+
+//jwb
+jwb.style.width = '30px'
+jwb.style.position = 'relative'
+jwb.style.left = '85px'
+jwb.style.top = '-40px'
+
+//jwb
+ml.style.width = '50px'
+ml.style.position = 'relative'
+ml.style.left = '85px'
+ml.style.top = '-40px'
+
+//ig
+/*ig.style.color = 'white'
+ig.style.position = 'relative'
+ig.style.right = ''
+ig.style.top = '10px'
+ig.style.hight = '0px'
+*/ 
+
+function contac() {
+  contak.style.display = 'inline'
+  btn.style.display = 'none'
+}
+
+function back() {
+  contak.style.display = 'none'
+  btn.style.display = 'inline'
+  kk.style.display = 'none'
+}
+
+function dd() {
+  kk.style.display = 'inline'
+ btn.style.display = 'none'
+}
+
+function str() {
+  store.style.display = 'inline'
+ btn.style.display = 'none'
+}
